73712475c6773bb8b1257e5dc2c07e84ccf3073b
Andrey Krisanov <develop4net@gmail.com>
2023-06-21 20:17:19 +0200

    chapter 2.1 (sequential containers): task 5 – editor with shift

--- 2.1_sequential_containers/2.1.4_text_editor.cpp
+++ 2.1_sequential_containers/2.1.4_text_editor.cpp
@@ -1,6 +1,6 @@
+#include <iostream>
 #include <list>
 #include <string>
-#include <iostream>
 #include <utility>
 
 int main()
@@ -23,28 +23,104 @@ int main()
 
     // define a cursor and the buffer
     auto current_line = lines.begin();
-    std::string buffer;
+    std::list<std::string> buffer;
+
+    bool shift_pressed = false;
+    int shift_offset = 0;
+    auto shift_line = lines.begin();
 
     // read and execute commands
     std::string command;
     while (std::cin >> command)
     {
-        if (command == "Down" && current_line != lines.end())
+        if (command == "Shift")
         {
-            ++current_line;
+            shift_pressed = true;
         }
-        if (command == "Up" && current_line != lines.begin())
+        if (command == "Up")
         {
+            if (current_line == lines.begin())
+            {
+                continue;
+            }
+
             --current_line;
+            if (!shift_pressed)
+            {
+                shift_line = current_line;
+            }
+            else
+            {
+                --shift_offset;
+            }
         }
-        if (command == "Ctrl+X" && current_line != lines.end())
+        if (command == "Down")
         {
-            buffer = std::move(*current_line);
-            current_line = lines.erase(current_line);
+            if (current_line == lines.end())
+            {
+                continue;
+            }
+
+            ++current_line;
+            if (!shift_pressed)
+            {
+                shift_line = current_line;
+            }
+            else
+            {
+                ++shift_offset;
+            }
         }
-        if (command == "Ctrl+V" && !buffer.empty())
+        if (command == "Ctrl+X")
         {
-            lines.insert(current_line, buffer);
+            if (shift_line == current_line && current_line == lines.end())
+            {
+                continue;
+            }
+
+            buffer.clear();
+
+            if (shift_line == current_line)
+            {
+                auto to_splice = current_line;
+                current_line = std::next(current_line);
+                buffer.splice(buffer.begin(), lines, to_splice);
+            }
+            else if (shift_offset < 0)
+            {
+                buffer.splice(buffer.begin(), lines, current_line, shift_line);
+                current_line = shift_line;
+            }
+            else
+            {
+                buffer.splice(buffer.begin(), lines, shift_line, current_line);
+            }
+
+            shift_pressed = false;
+            shift_line = current_line;
+            shift_offset = 0;
+        }
+        if (command == "Ctrl+V")
+        {
+            if (buffer.empty())
+            {
+                continue;
+            }
+
+            if (shift_offset < 0)
+            {
+                current_line = lines.erase(current_line, shift_line);
+            }
+            else if (shift_offset > 0)
+            {
+                current_line = lines.erase(shift_line, current_line);
+            }
+
+            lines.insert(current_line, buffer.begin(), buffer.end());
+
+            shift_pressed = false;
+            shift_line = current_line;
+            shift_offset = 0;
         }
     }
 
